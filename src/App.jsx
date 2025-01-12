817433b7cb1d6bbe3e4a15e1ca6f0c4d784b743a
Ігор Гавриленко <igor.gavrylenko05@gmail.com>
2025-01-12 22:09:38 +0100

    Added props to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from "./data";
+
+
 const reactDescriptions = ['Fundamential', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -9,7 +13,7 @@ function Header() {
 
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {description} React concepts you will need for almost any app you are
@@ -19,11 +23,30 @@ function Header() {
     );
 }
 
+function CoreConcept({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt=""></img>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]}/>
+                        <CoreConcept {...CORE_CONCEPTS[1]}/>
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
